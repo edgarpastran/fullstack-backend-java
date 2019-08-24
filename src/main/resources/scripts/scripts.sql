9c60708365aaac43fbe73ffd8fb5a835ad7c5feb
Edgar Pastran <edgarpastran@gmail.com>
2019-08-24 01:34:52 +0100

    Adding the security frame

--- src/main/resources/scripts/scripts.sql
+++ src/main/resources/scripts/scripts.sql
@@ -21,10 +21,20 @@ END; $$
 LANGUAGE 'plpgsql';
 
 
+-- DATA INTO USER_ACCOUNT TABLE
+INSERT INTO user_account(id_user, username, password, enabled) VALUES (1, 'admin-edpas@dispostable.com', '$2a$10$XGCIOV1b.7BbMnheuxK57..aO8ZhKLcErqXYnRACiAN8gMHc9rKhy', true);
+INSERT INTO user_account(id_user, username, password, enabled) VALUES (2, 'manager-edpas@dispostable.com', '$2a$10$bP9tlqeCi.QzjfW8LphwaefX6LrjQ6XBy7dju6O3e/1bX5NB6Be9O', true);
+INSERT INTO user_account(id_user, username, password, enabled) VALUES (3, 'operator-edpas@dispostable.com', '$2a$10$iEjhS2fzBgejlvut8xx1IuyF7dogguEDdl8qym0ju60MO1cfQwiyC', true);
+
 -- DATA INTO ROLE TABLE
 INSERT INTO role(id_role, name, description) VALUES (1, 'Administrator', 'Administrator');
 INSERT INTO role(id_role, name, description) VALUES (2, 'Manager', 'Manager');
 INSERT INTO role(id_role, name, description) VALUES (3, 'Operator', 'Operator');
+
+-- DATA INTO USER_ROLE TABLE
+INSERT INTO user_role(id_user, id_role) VALUES (1, 1);
+INSERT INTO user_role(id_user, id_role) VALUES (2, 2);
+INSERT INTO user_role(id_user, id_role) VALUES (3, 3);
 
 -- DATA INTO MENU TABLE
 INSERT INTO menu(id_menu, name, icon, url) VALUES (1, 'Search', 'search', '/search');
